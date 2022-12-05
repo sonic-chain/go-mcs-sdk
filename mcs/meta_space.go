5148fb4a5b715c05196950ff51f5b09889dca0b6
zzq <zzhao@nbai.io>
2022-12-05 14:56:48 -0500

    add get bucket info and bucket id api

--- mcs/meta_space.go
+++ mcs/meta_space.go
@@ -5,12 +5,14 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 	"go-mcs-sdk/mcs/common"
 	"log"
+	"time"
+	"unsafe"
 )
 
 const (
 	McsBackendBaseUrl               = "http://192.168.199.61:8889/api/"
 	UserWalletAddressForRegisterMcs = "0x7d2C017e20Ee3D86047727197094fCD197656168"
-	UserWalletAddressPK             = ""
+	UserWalletAddressPK             = "9197b7d31cb4548aa4bba82d3a15bdf9f35814d130e9077b4b0ed8a7235addbe"
 	ChainNameForRegisterOnMcs       = "polygon.mumbai"
 )
 
@@ -51,13 +53,86 @@ func (client *MetaSpaceClient) GetToken() error {
 	return nil
 }
 
-func (client *MetaSpaceClient) GetBuckets() error {
+func (client *MetaSpaceClient) GetBuckets() ([]byte, error) {
 	httpRequestUrl := client.MetaSpaceUrl + common.DIRECTORY
-	bytes, err := common.HttpGet(httpRequestUrl, client.JwtToken, nil)
+	bucketListInfoBytes, err := common.HttpGet(httpRequestUrl, client.JwtToken, nil)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	log.Println(*(*string)(unsafe.Pointer(&bucketListInfoBytes)))
+	return bucketListInfoBytes, nil
+}
+
+func (client *MetaSpaceClient) GetBucketInfoByBucketName(bucketName string) error {
+	httpRequestUrl := client.MetaSpaceUrl + common.DIRECTORY + "/" + bucketName
+	bucketListInfoBytes, err := common.HttpGet(httpRequestUrl, client.JwtToken, nil)
 	if err != nil {
 		log.Println(err)
 		return err
 	}
-	log.Println(bytes)
+	log.Println(*(*string)(unsafe.Pointer(&bucketListInfoBytes)))
 	return err
+}
+
+func (client *MetaSpaceClient) GetBucketIDByBucketName(bucketName string) (string, error) {
+	response, err := client.GetBuckets()
+	bucketId := ""
+	if err != nil {
+		log.Println(err)
+		return bucketId, err
+	}
+	//var objectList *ObjectList
+	var dict map[string]interface{}
+	err = json.Unmarshal(response, &dict)
+	if err != nil {
+		log.Println(err)
+		return bucketId, err
+	}
+	log.Println(dict)
+	dataInReturn := dict["data"].(map[string]interface{})
+	objectInData := dataInReturn["objects"].([]interface{})
+	for _, v := range objectInData {
+		vObject := v.(map[string]interface{})
+		bucketNameInReturn := vObject["name"].(string)
+		if bucketName == bucketNameInReturn {
+			bucketId = vObject["id"].(string)
+		}
+	}
+	return bucketId, nil
+}
+
+type ObjectList struct {
+	Parent        string         `json:"parent,omitempty"`
+	Objects       []*Object      `json:"objects"`
+	Policy        *PolicySummary `json:"policy,omitempty"`
+	FolderCnt     int64          `json:"folder_cnt"`
+	FreeFolderCnt int64          `json:"free_folder_cnt"`
+	FileCnt       int64          `json:"file_cnt"`
+}
+
+// Object 文件或者目录
+type Object struct {
+	ID            string    `json:"id"`
+	Name          string    `json:"name"`
+	Path          string    `json:"path"`
+	Pic           string    `json:"pic"`
+	Size          uint64    `json:"size"`
+	Type          string    `json:"type"`
+	PayloadCid    string    `json:"payload_cid"`
+	IpfsUrl       string    `json:"ipfs_url"`
+	PinStatus     string    `json:"pin_status"`
+	Date          time.Time `json:"date"`
+	FilesCount    int       `json:"files_count"`
+	CreateDate    time.Time `json:"create_date"`
+	UpdateDate    time.Time `json:"update_date"`
+	Key           string    `json:"key,omitempty"`
+	SourceEnabled bool      `json:"source_enabled"`
+}
+type PolicySummary struct {
+	ID       string   `json:"id"`
+	Name     string   `json:"name"`
+	Type     string   `json:"type"`
+	MaxSize  uint64   `json:"max_size"`
+	FileType []string `json:"file_type"`
 }
--- mcs/meta_space_test.go
+++ mcs/meta_space_test.go
@@ -3,10 +3,12 @@ package mcs
 import (
 	"log"
 	"testing"
+	"unsafe"
 )
 
 const (
 	MetaSpaceBackendBaseUrl = "http://192.168.199.61:5212/api/"
+	BucketNameForTest       = "zzq-1105"
 )
 
 func TestMetaSpaceGetBuckets(t *testing.T) {
@@ -15,8 +17,35 @@ func TestMetaSpaceGetBuckets(t *testing.T) {
 	if err != nil {
 		log.Println(err)
 	}
-	err = metaClient.GetBuckets()
+	resp, err := metaClient.GetBuckets()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println(*(*string)(unsafe.Pointer(&resp)))
+}
+
+func TestMetaSpaceGetBucketInfo(t *testing.T) {
+	metaClient := NewMetaSpaceClient(MetaSpaceBackendBaseUrl)
+	err := metaClient.GetToken()
 	if err != nil {
 		log.Println(err)
 	}
+	err = metaClient.GetBucketInfoByBucketName(BucketNameForTest)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func TestMetaSpaceGetBucketID(t *testing.T) {
+	metaClient := NewMetaSpaceClient(MetaSpaceBackendBaseUrl)
+	err := metaClient.GetToken()
+	if err != nil {
+		log.Println(err)
+	}
+	bucketId, err := metaClient.GetBucketIDByBucketName(BucketNameForTest)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println(bucketId)
 }
