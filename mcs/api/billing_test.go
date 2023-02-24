04f09a3f7ef731c4991cc9bdc83bbd8ee39d042b
DoraNebula <90062637+DoraNebula@users.noreply.github.com>
2023-02-24 14:54:51 -0500

    Update billing_test.go

--- mcs/api/billing_test.go
+++ mcs/api/billing_test.go
@@ -41,3 +41,22 @@ func TestGetLockPaymentInfo(t *testing.T) {
 
 	logs.GetLogger().Info(*lockPaymentInfo)
 }
+
+func TestGetBillingHistory(t *testing.T) {
+	mcsClient, err := GetMcsClient()
+	if err != nil {
+		logs.GetLogger().Error(err)
+		return
+	}
+
+	logs.GetLogger().Info(mcsClient)
+
+	billingHistoryParams := BillingHistoryParams{}
+	billing, recCnt, err := mcsClient.GetBillingHistory(billingHistoryParams)
+	if err != nil {
+		logs.GetLogger().Error(err)
+		return
+	}
+
+	logs.GetLogger().Info(billing, recCnt)
+}
