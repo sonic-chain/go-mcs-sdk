7af788365925c9efb09307f2f93101c8edb7d735
DoraNebula <90062637+DoraNebula@users.noreply.github.com>
2023-02-28 14:00:17 -0500

    get deals to sign

--- mcs/api/dao.go
+++ mcs/api/dao.go
@@ -45,3 +45,46 @@ func (mcsCient *MCSClient) GetDeals2PreSign() ([]*Deal2PreSign, error) {
 
 	return getDeals2PreSignResponse.Data, nil
 }
+
+type Deal2SignBatchInfo struct {
+	BatchNo int      `json:"batch_no"`
+	WCid    []string `json:"w_cid"`
+}
+
+type Deal2Sign struct {
+	OfflineDealId int64                 `json:"offline_deal_id"`
+	CarFileId     int64                 `json:"car_file_id"`
+	DealId        int64                 `json:"deal_id"`
+	BatchCount    int                   `json:"batch_count"`
+	BatchSizeMax  int                   `json:"batch_size_max"`
+	BatchInfo     []*Deal2SignBatchInfo `json:"batch_info"`
+}
+
+type GetDeals2SignResponse struct {
+	Response
+	Data []*Deal2Sign `json:"data"`
+}
+
+func (mcsCient *MCSClient) GetDeals2Sign() ([]*Deal2Sign, error) {
+	apiUrl := libutils.UrlJoin(mcsCient.BaseUrl, constants.API_URL_DAO_GET_DEALS_2_SIGN)
+	result, err := web.HttpGet(apiUrl, mcsCient.JwtToken, nil)
+	if err != nil {
+		logs.GetLogger().Error(err)
+		return nil, err
+	}
+
+	var getDeals2SignResponse GetDeals2SignResponse
+	err = json.Unmarshal(result, &getDeals2SignResponse)
+	if err != nil {
+		logs.GetLogger().Error(err)
+		return nil, err
+	}
+
+	if !strings.EqualFold(getDeals2SignResponse.Status, constants.HTTP_STATUS_SUCCESS) {
+		err := fmt.Errorf("get parameters failed, status:%s,message:%s", getDeals2SignResponse.Status, getDeals2SignResponse.Message)
+		logs.GetLogger().Error(err)
+		return nil, err
+	}
+
+	return getDeals2SignResponse.Data, nil
+}
--- mcs/api/dao_test.go
+++ mcs/api/dao_test.go
@@ -23,3 +23,21 @@ func TestGetDeals2PreSign(t *testing.T) {
 		logs.GetLogger().Info(*deal2Sign)
 	}
 }
+
+func TestGetDeals2Sign(t *testing.T) {
+	mcsClient, err := GetMcsClient()
+	if err != nil {
+		logs.GetLogger().Error(err)
+		return
+	}
+
+	deals2PreSign, err := mcsClient.GetDeals2Sign()
+	if err != nil {
+		logs.GetLogger().Error(err)
+		return
+	}
+
+	for _, deal2Sign := range deals2PreSign {
+		logs.GetLogger().Info(*deal2Sign)
+	}
+}
