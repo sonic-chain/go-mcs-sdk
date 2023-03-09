a5168d088f482fa32acc7e3122cd317174a5a5de
DoraNebula <90062637+DoraNebula@users.noreply.github.com>
2023-03-09 16:24:32 -0500

    auth

--- mcs/api/common/constants/constants.go
+++ mcs/api/common/constants/constants.go
@@ -21,6 +21,7 @@ const (
 	API_URL_USER_LOGIN           = "/api/v1/user/login_by_metamask_signature"
 	API_URL_USER_GENERATE_APIKEY = "/api/v1/user/generate_api_key"
 	API_URL_USER_LOGIN_BY_APIKEY = "/api/v1/user/login_by_api_key"
+	API_URL_USER_CHECK_LOGIN     = "/api/v1/user/check_login"
 
 	API_URL_BILLING_HISTORY          = "/api/v1/billing"
 	API_URL_BILLING_FILECOIN_PRICE   = "/api/v1/billing/price/filecoin"
