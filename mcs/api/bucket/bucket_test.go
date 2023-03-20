2d9a19cd7d42b9dc9c6767351130878950490ee9
DoraNebula <90062637+DoraNebula@users.noreply.github.com>
2023-03-20 09:59:20 -0400

    test

--- mcs/api/bucket/bucket_test.go
+++ mcs/api/bucket/bucket_test.go
@@ -4,22 +4,22 @@ import (
 	"testing"
 
 	"go-mcs-sdk/mcs/api/common/logs"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCreateBucket(t *testing.T) {
 	bucketUid, err := buketClient.CreateBucket("test23")
-	if err != nil {
-		logs.GetLogger().Fatal(err)
-	}
+	assert.Nil(t, err)
+	assert.NotEmpty(t, bucketUid)
 
 	logs.GetLogger().Info(*bucketUid)
 }
 
 func TestGetBuckets(t *testing.T) {
 	buckets, err := buketClient.GetBuckets()
-	if err != nil {
-		logs.GetLogger().Fatal(err)
-	}
+	assert.Nil(t, err)
+	assert.NotEmpty(t, buckets)
 
 	for _, bucket := range buckets {
 		logs.GetLogger().Info(*bucket)
@@ -28,23 +28,18 @@ func TestGetBuckets(t *testing.T) {
 
 func TestDeleteBucket(t *testing.T) {
 	err := buketClient.DeleteBucket("7bb5d325-e31c-486d-8420-169067dc401b")
-	if err != nil {
-		logs.GetLogger().Fatal(err)
-	}
+	assert.Nil(t, err)
 }
 
 func TestRenameBucket(t *testing.T) {
 	err := buketClient.RenameBucket("tests", "a7303d2a-acd2-48ac-a062-8454bbf148d2")
-	if err != nil {
-		logs.GetLogger().Fatal(err)
-	}
+	assert.Nil(t, err)
 }
 
 func TestGetTotalStorageSize(t *testing.T) {
 	totalStorageSize, err := buketClient.GetTotalStorageSize()
-	if err != nil {
-		logs.GetLogger().Fatal(err)
-	}
+	assert.Nil(t, err)
+	assert.NotEmpty(t, totalStorageSize)
 
 	logs.GetLogger().Info(*totalStorageSize)
 }
